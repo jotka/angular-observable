7ba17dd8a5d524ff168bf6194c5c7a958895ceca
jotka <n45nokia>
2018-08-14 11:17:48 +0200

    Initial commit

---
+++ src/app/customer.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Customer } from './customer';
+import { CUSTOMERS } from './mock-customers';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CustomerService {
+
+    constructor() {
+    }
+
+    getCustomers():Observable<Customer[]> {
+        return of(CUSTOMERS);
+    }
+}
